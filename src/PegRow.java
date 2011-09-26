d79b5bf2580d99c4dcdbadd481568dc3c2fc6242
infectedwithdrew@gmail.com <infectedwithdrew@gmail.com@44802c89-8691-a55c-e98e-ed8e11a5b231>
2011-09-26 00:08:27 +0000

    PegRow now has compareGuess to compare the guess in that row to the solution.

--- src/PegRow.java
+++ src/PegRow.java
@@ -23,5 +23,45 @@ public class PegRow {
 		this.puzzlePegs = new ArrayList<PuzzlePeg>();
 		this.feedbackPegs = new ArrayList<FeedbackPeg>();
 	}
+	
+	/**
+	 * Compare the guess in this row to the solution.
+	 * @param solution
+	 * @return true if solution equals this row's puzzle pegs.
+	 */
+	public boolean compareGuess(ArrayList<PuzzlePeg> solution) {
+		return (solution.get(0).color == puzzlePegs.get(0).color
+				&& solution.get(1).color == puzzlePegs.get(1).color
+				&& solution.get(2).color == puzzlePegs.get(2).color
+				&& solution.get(3).color == puzzlePegs.get(3).color);
+	}
+
+	/**
+	 * @return the puzzlePegs
+	 */
+	public ArrayList<PuzzlePeg> getPuzzlePegs() {
+		return puzzlePegs;
+	}
+
+	/**
+	 * @return the feedbackPegs
+	 */
+	public ArrayList<FeedbackPeg> getFeedbackPegs() {
+		return feedbackPegs;
+	}
+
+	/**
+	 * @param puzzlePegs the puzzlePegs to set
+	 */
+	public void setPuzzlePegs(ArrayList<PuzzlePeg> puzzlePegs) {
+		this.puzzlePegs = puzzlePegs;
+	}
+
+	/**
+	 * @param feedbackPegs the feedbackPegs to set
+	 */
+	public void setFeedbackPegs(ArrayList<FeedbackPeg> feedbackPegs) {
+		this.feedbackPegs = feedbackPegs;
+	}
 
 }
