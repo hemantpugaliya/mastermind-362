473d5ca3d9961cdcc557390df8add53c8897d85c
mmo2912@rit.edu <mmo2912@rit.edu@44802c89-8691-a55c-e98e-ed8e11a5b231>
2011-10-18 13:58:18 +0000

    HumanCodeBreaker

---
+++ src/HumanCodeMaker.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+import java.util.ArrayList;
+/**
+ * HumanCodeMaker
+ * The player choosing the solution and providing feedback after each guess
+ * @author Gabbie Burns
+ *
+ */
+
+
+
+public class HumanCodeMaker extends CodeMaker {
+	/**
+	 * Stub because human feedback information comes from the UI
+	 */
+	 public ArrayList<PegColor> makeMove(){
+		 
+		 	return null;
+	 }
+
+}
+
